bdc77b5b5a74c03541cb6e19a3dcbad8fded4397
artemlenskih <artemlenskih@gmail.com>
2026-05-12 21:56:37 +0700

    Готовое резюме

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
-import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+document.getElementById('download-pdf').addEventListener('click', () => {
+    const element = document.getElementById('resume');
+    
+    // Клонируем элемент, чтобы манипуляции не были видны на экране
+    const clone = element.cloneNode(true);
+    
+    // Создаем временный контейнер, прижатый к углу, чтобы избежать обрезки
+    const container = document.createElement('div');
+    container.style.position = 'fixed';
+    container.style.left = '-9999px'; // Прячем за экран
+    container.style.top = '0';
+    container.style.width = '210mm'; // Жестко задаем ширину A4
+    container.appendChild(clone);
+    document.body.appendChild(container);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    const opt = {
+        margin: [10, 10, 10, 10],
+        filename: 'resume.pdf',
+        image: { type: 'jpeg', quality: 1 },
+        html2canvas: { 
+            scale: 2, 
+            useCORS: true,
+            logging: false,
+            width: element.offsetWidth, // Берем реальную ширину
+            scrollY: 0,
+            scrollX: 0
+        },
+        jsPDF: { 
+            unit: 'mm', 
+            format: 'a4', 
+            orientation: 'portrait' 
+        }
+    };
 
-setupCounter(document.querySelector('#counter'))
+    // Печатаем клон и удаляем его
+    html2pdf().set(opt).from(clone).save().then(() => {
+        document.body.removeChild(container);
+    });
+});
